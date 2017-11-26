3a6c66da7e4637454f09325ca00d0b7ecb1282f2
Zhaopeng Wang <zpw1992>
2017-11-26 14:44:32 -0600

    modify the style

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -24,10 +24,44 @@
 			<li><a href="#">Shopping Carts</a></li>
 			<li><a href="#">Contact Us</a></li>
 		</ul>
+		
+		<div id="form">
+			<form method="get" action="resuls.php" enctype="mutlpart/form-data">
+				<input type="text" name="user_query" placeholder="Search for a Product"/>
+				<input type="submit" name="search" value="Search"/>
+			</form>
+		</div><!--end of form div-->
+		
 	</div><!--navigation bar ends-->
 	
 	<div class="content_wrapper">
-		<div id="sidebar">This is sidebar</div>
+		<div id="sidebar">
+			<div id="sidebar_title">Categories</div>
+			<ul id="cats">
+				<li><a href="#">Laptops</a></li>
+				<li><a href="#">Computers</a></li>
+				<li><a href="#">Cell Phones</a></li>
+				<li><a href="#">Cameras</a></li>
+				<li><a href="#">Tablets</a></li>
+			
+			<ul>
+			
+			<div id="sidebar_title">Brands</div>
+			
+			<ul id="cats">
+				<li><a href="#">Apple</a></li>
+				<li><a href="#">Asus</a></li>
+				<li><a href="#">Dell</a></li>
+				<li><a href="#">HP</a></li>
+				<li><a href="#">Lenovo</a></li>
+				<li><a href="#">Samsung</a></li>
+			
+			<ul>
+			
+			
+		</div>
+		
+		
 		<div id="content_area">This is content area</div>
 	</div>
 	<div id="footer">This is the footbar</div>
