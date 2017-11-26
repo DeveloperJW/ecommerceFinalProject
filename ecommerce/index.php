d8ceb1e1b4ccd5ab74e7615212f6889d7da37939
Zhaopeng Wang <zpw1992>
2017-11-26 03:25:21 -0600

    Set up template and css style

---
+++ ecommerce/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE>
+<html>
+	<head>
+	<title>My Online Shop</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	</head>
+	
+<body>
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+	
+	
+	<div class="header_wrapper">
+	<img id="logo" src="images/logo.png"/>
+	</div>
+	<!--Header ends here-->
+	<!--Navigation bar starts here-->
+	<div class="menubar">
+		<ul id="menu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">All Products</a></li>
+			<li><a href="#">My Account</a></li>
+			<li><a href="#">Sign Up</a></li>
+			<li><a href="#">Shopping Carts</a></li>
+			<li><a href="#">Contact Us</a></li>
+		</ul>
+	</div><!--navigation bar ends-->
+	
+	<div class="content_wrapper">
+		<div id="sidebar">This is sidebar</div>
+		<div id="content_area">This is content area</div>
+	</div>
+	<div id="footer">This is the footbar</div>
+	
+	</div><!--Main Container ends here-->
+	
+</body>
+</html>
