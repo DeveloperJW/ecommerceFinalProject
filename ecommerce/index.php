3829200c15604959dd6130b498ab0132514005f9
Zhaopeng Wang <zpw1992>
2017-11-27 21:18:30 -0600

    Update: implement the function of search bar

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -16,22 +16,23 @@ include("functions/functions.php");
 	
 	
 	<div class="header_wrapper">
-	<img id="logo" src="images/logo.png"/>
+	<!--click the logo top left corner and direct to homepage-->
+	<a href="index.php"><img id="logo" src="images/logo.png"/></a>
 	</div>
 	<!--Header ends here-->
 	<!--Navigation bar starts here-->
 	<div class="menubar">
 		<ul id="menu">
-			<li><a href="#">Home</a></li>
-			<li><a href="#">All Products</a></li>
-			<li><a href="#">My Account</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="all_products.php">All Products</a></li>
+			<li><a href="customer/my_account.php">My Account</a></li>
 			<li><a href="#">Sign Up</a></li>
-			<li><a href="#">Shopping Carts</a></li>
+			<li><a href="cart.php">Shopping Carts</a></li>
 			<li><a href="#">Contact Us</a></li>
 		</ul>
 		
 		<div id="form">
-			<form method="get" action="resuls.php" enctype="mutlpart/form-data">
+			<form method="get" action="results.php" enctype="mutlpart/form-data">
 				<input type="text" name="user_query" placeholder="Search for a Product"/>
 				<input type="submit" name="search" value="Search"/>
 			</form>
@@ -61,8 +62,15 @@ include("functions/functions.php");
 		
 		
 		<div id="content_area">
+		<div id="shopping_cart">
+			<span style="float:right;font-size:18px; padding: 5px; line-height:40px;">Welcome Guest!
+			
+			</span>
+		</div>
 			<div id="products_box">
 			<?php getPro();?>
+			<?php getCatPro();?>
+			<?php getBrandPro();?>
 			
 			</div>
 		
